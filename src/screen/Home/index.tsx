64b363cc6beb03b8e66a05a23f1bd53321d76c7f
walacefex <walacefex01@gmail.com>
2023-07-11 12:40:35 -0300

    Creating button with function onPress and print in console

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,7 +1,12 @@
-import { Text, TextInput, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import {styles} from './styles';
 
 export  function Home() {
+
+  function handleParticipantAdd() {
+    console.log('ok')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
@@ -11,6 +16,11 @@ export  function Home() {
         placeholder="Nome do Participante"
         placeholderTextColor="#6B6B6B"	
       />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screen/Home/styles.ts
+++ src/screen/Home/styles.ts
@@ -23,6 +23,19 @@ export const styles = StyleSheet.create({
     color: '#FFF',
     padding: 16,
     fontSize: 16,
-    
+    marginTop: 24,
+  },
+  button:{
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 24,
   }
+
 });
