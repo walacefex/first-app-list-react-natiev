88a4e80032ebc4bca9139c376769d10dabdc7e34
walacefex <walacefex01@gmail.com>
2023-07-11 15:07:26 -0300

    Adding statusbar and define styles to her

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
+import { StatusBar } from "react-native";
 import {Home} from "./src/screen/Home";
 
 export default function App() {
   return (
-   <Home />
+    <>
+      <StatusBar
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   );
 }
 
